97ff03bbf433bb7887e18f367035d43c32582ab4
Daniel Trnka <daniel.trnka@gmail.com>
2024-11-10 21:10:17 +0100

    LIN: simple LED animation

--- src/lin_master.rs
+++ src/lin_master.rs
@@ -8,6 +8,11 @@ use embassy_time::{with_timeout, Duration, Timer};
 use embedded_io_async::{Read, Write};
 use lin_bus::{Frame, PID};
 
+const LIN_FRAME_OFFSET: u8 = 5;
+const LIN_FRAME_RGB: u8 = LIN_FRAME_OFFSET;
+const LIN_FRAME_LEDS: u8 = 1 + LIN_FRAME_OFFSET;
+const LIN_FRAME_PHOTORES: u8 = 2 + LIN_FRAME_OFFSET;
+
 pub struct LinMaster {
     pub driver: BufferedUart<'static>,
 }
@@ -98,15 +103,26 @@ impl LinMaster {
 
 #[task]
 pub async fn lin_scheduler(mut lin: LinMaster) {
+    let mut led = 1u8;
     loop {
-        let f = lin_bus::Frame::from_data(PID::from_id(0x22), &[0x11, 0x22, 0x33, 0x44]);
+        let f = lin_bus::Frame::from_data(PID::from_id(LIN_FRAME_LEDS), &[led]);
+        lin.write_frame(&f).await.unwrap();
+
+        led = (led * 2) & 0xF;
+        if led == 0 {
+            led = 1;
+        }
+
+        let f = lin_bus::Frame::from_data(PID::from_id(LIN_FRAME_RGB), &[255, 0, 0]);
         lin.write_frame(&f).await.unwrap();
 
         Timer::after_millis(100).await;
 
-        let fr = lin.read_frame(PID::from_id(0x08), 4).await;
+        let fr = lin.read_frame(PID::from_id(LIN_FRAME_PHOTORES), 2).await;
         match fr {
-            Ok(fr) => info!("LIN RX {} {:?}", fr.get_pid().get_id(), fr.get_data()),
+            Ok(fr) => {
+                info!("LIN RX {} {:?}", fr.get_pid().get_id(), fr.get_data())
+            }
             Err(err) => info!(
                 "Error reading LIN: {}",
                 match err {
